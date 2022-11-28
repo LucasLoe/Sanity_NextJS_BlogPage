13f609e4385d5444c96ef7de285ed2b37c76b752
Lucas L <lucas_loe@gmx.de>
2022-11-28 19:03:50 +0100

    some updateds

--- sanity_api/schemas/blockContent.js
+++ sanity_api/schemas/blockContent.js
@@ -1,3 +1,5 @@
+import link2 from "./link2"
+
 /**
  * This is the schema definition for the rich text fields used for
  * for this blog studio. When you import it in schemas.js it can be
--- sanity_api/schemas/schema.js
+++ sanity_api/schemas/schema.js
@@ -9,6 +9,9 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import pageCategory from './pageCategory'
+import pageContent from './pageContent'
+import gallery from './gallery'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -22,6 +25,9 @@ export default createSchema({
     post,
     author,
     category,
+    pageCategory,
+    pageContent,
+    gallery,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
